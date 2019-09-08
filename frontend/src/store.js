52107a1cc238628e757dcddf692f71dd3b23681e
Ellarddekoeijer <ellarddekoeijer@gmail.com>
2019-09-08 11:46:30 +0200

    disabled redux extension

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,6 +7,6 @@ export default createStore(
         applyMiddleware(
             thunk
         ),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     )
 );
